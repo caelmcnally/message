49d06bd87d32ff9667c4ba18df27a8570bc3a7e4
MrFr0sty <cael@caels-mbp.home>
2020-10-22 19:18:08 +0100

    Ids now unique, update, get, delete work correctly

---
+++ fucking.js
@@ -0,0 +1,6 @@
+var greetings = ["Hello", "Sup", "Hola", "greetings", "Good morrow", 'howzit'];
+var names = ['tom','cael','fucker', 'lil bitch', 'cvnt', 'ya big bastard'];
+
+// console.log( greetings[Math.floor(Math.random()*greetings.length)], name[Math.floor(Math.random()*name.length)] )
+
+//====> fucking random
